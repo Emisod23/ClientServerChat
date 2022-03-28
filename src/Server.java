622304412ed0da85766c9850f1ef7c0c0660ba39
Hera-Heeto <Emil.soderlund@elev.ga.ntig.se>
2022-03-28 08:54:09 +0200

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author magnus
+ */
+public class Server {
+    public static void main(String[] args){
+        int port = 2323;
+        boolean run = true;
+        ServerSocket serverSocket;
+        Socket socket;
+        System.out.println("Server started.");
+
+        try {
+            serverSocket = new ServerSocket(port);
+            while (true) {
+                System.out.println("Waiting for connections!");
+                socket = serverSocket.accept();
+                // Go
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                ListenerThread in = new ListenerThread(new BufferedReader(new InputStreamReader(socket.getInputStream())));
+                Thread listener = new Thread(in);
+                listener.start();
+                System.out.println("Client connected!");
+                Scanner tgb = new Scanner(System.in);
+                //Protocol
+                while (run) {
+                    String msg = tgb.nextLine();
+                    out.println("SERVER: " + msg);
+                }
+                out.close();
+                socket.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Server fail");
+        }
+    }
+}
